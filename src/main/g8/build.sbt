7c90e25aa0217b2cb74c8b60326a7cecea0bc84f
Lincoln Stoll <lstoll@lstoll.net>
2011-09-11 13:44:08 +1000

    Make Example Spec work out of the box.

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -6,7 +6,10 @@ version := "$version$"
 
 libraryDependencies ++= Seq(
    "net.databinder" %% "unfiltered-netty" % "$unfiltered_version$",
-   "org.clapper" %% "avsl" % "0.3.1"
+   "org.clapper" %% "avsl" % "0.3.1",
+   "net.databinder" %% "unfiltered-spec" % "$unfiltered_version$" % "test",
+   "org.scala-tools.testing" %% "specs" % "1.6.8" % "test",
+   "net.databinder" %% "dispatch-http" % "0.8.5" % "test"
 )
 
 resolvers ++= Seq(
--- src/main/g8/src/test/scala/ExampleSpec.scala
+++ src/main/g8/src/test/scala/ExampleSpec.scala
@@ -4,15 +4,15 @@ import org.specs._
 
 import dispatch._
 
-object ExampleSpec extends Specification with unfiltered.spec.Served {
+object ExampleSpec extends Specification with unfiltered.spec.netty.Served {
   
   import dispatch._
   
-  def setup = { _.filter(new App) }
+  def setup = { unfiltered.netty.Http(_).handler(new App) }
   
   "The example app" should {
     "serve unfiltered text" in {
-      Http(host as_str) must_=="unfiltered example"
+      Http(host as_str) must beMatching("What say you")
     }
   }
 }
