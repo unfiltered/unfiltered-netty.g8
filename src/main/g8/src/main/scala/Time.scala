95ada0433b84441f61612cb711d759df590a5583
Nathan Hamblen <nathan@technically.us>
2013-11-24 23:15:06 -0500

    using unfiltered's new future.Plan

--- src/main/g8/src/main/scala/Time.scala
+++ src/main/g8/src/main/scala/Time.scala
@@ -5,30 +5,34 @@ import unfiltered.response._
 
 import unfiltered.netty._
 
+import scala.concurrent.{ExecutionContext,Future}
+
 /** Asynchronous plan that gets the time in a ridiculous fashion.
  *  (But imagine that it's using a vital external HTTP service to
  *  inform its response--this is a fine way to do that.) */
-object Time extends async.Plan
+object Time extends future.Plan
   with ServerErrorResponse {
   val logger = org.clapper.avsl.Logger(getClass)
+
+  implicit def executionContext = ExecutionContext.Implicits.global
   
   def intent = {
     case req @ GET(Path("/time")) => 
       logger.debug("GET /time")
-      import dispatch._, Defaults._
+      import dispatch._
       // the call below is non-blocking, so we return quickly
       // and free netty's worker thread
       for {
         time <- Http(host("127.0.0.1", 8080).POST / "time" > as.String)
-      } {
-        // later, we respond to the request
-        req.respond(view(time))
+      } yield {
+        // in a future, we respond to the request
+        view(time)
       }
     case req @ POST(Path("/time")) =>
       logger.debug("POST /time")
       // since we don't have to do any blocking IO for this request
-      // we can call respond right way
-      req.respond(ResponseString(new java.util.Date().toString))
+      // we can respond immediately
+      Future.successful(ResponseString(new java.util.Date().toString))
   }
   def view(time: String) = {
     Html(
